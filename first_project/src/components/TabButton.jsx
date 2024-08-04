70521ad737ee9de63812ffcef3f0e952bd4dc546
djairmouad <154635690+djairmouad@users.noreply.github.com>
2024-08-04 09:09:47 -0700

    Delete first_project directory

--- first_project/src/components/TabButton.jsx
+++
@@ -1,5 +0,0 @@
-export default function TabButton({children,onSelect,isselected}){
-    return(
-        <li><button className={isselected ?"active":undefined} onClick={onSelect}>{children}</button></li>
-    )
-}
